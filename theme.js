03ccf771b6a475b92949875b34f90e120536ef11
urgosxd <60458146+urgosxd@users.noreply.github.com>
2020-07-27 22:55:57 -0500

    asd

---
+++ theme.js
@@ -0,0 +1,11 @@
+const theme = {
+  fonts: {},
+  colors: {},
+  devices: {
+    phone: "(max-width: 768px)",
+    tablet: "(max-width: 991px)",
+    desktop: "(min-width: 991px )",
+  },
+}
+
+export default theme
